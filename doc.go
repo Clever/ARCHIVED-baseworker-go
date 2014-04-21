3a11ebf62d9cb16eb8e1d2ebed9632e0c347ff73
Alex Zylman <alex.zylman@getclever.com>
2014-04-21 15:42:35 -0700

    add doc.go

---
+++ doc.go
@@ -0,0 +1,26 @@
+/*
+Package gearman provides a simple wrapper around a Gearman worker, based on
+http://godoc.org/github.com/mikespook/gearman-go.
+
+Usage
+
+Here's an example program that just listens for "test" jobs and logs the data that it receives:
+
+        package main
+
+        import(
+            "github.com/Clever/baseworker-go"
+            "log"
+        )
+
+        func jobFunc(job gearman.Job) ([]byte, error) {
+            log.Printf("Got job with data %s", job.Data())
+            return []byte{}, nil
+        }
+
+        func main() {
+            worker := gearman.New("test", jobFunc)
+            worker.Listen("localhost", "4730")
+        }
+*/
+package gearman
