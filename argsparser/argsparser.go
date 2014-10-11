0ca4329fc16d37d4029b8460d30ae47be85bb345
Dan Sparks <dan.sparks@clever.com>
2014-10-11 11:08:10 -0700

    Add test for blob hack

--- argsparser/argsparser.go
+++ argsparser/argsparser.go
@@ -1,9 +1,12 @@
 package argsparser
 
 import (
+	"crypto/md5"
+	"fmt"
 	"io/ioutil"
 	"os/exec"
 	"strings"
+	"unicode"
 )
 
 // ParseArgs converts the command line specified into a slice of the command line arguments.
@@ -18,6 +21,22 @@ func ParseArgs(commandline string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// BEGIN Uber super hack to fix failures on large payloads - replace long blocks of text
+	isAlphanumeric := func(c rune) bool {
+		return !(unicode.IsLetter(c) || unicode.IsNumber(c))
+	}
+	substitutions, contiguousStrings := map[string]string{}, strings.FieldsFunc(commandline, isAlphanumeric)
+	for i, str := range contiguousStrings {
+		if len(str) > 250 {
+			substitutions[fmt.Sprintf("%x%d", md5.Sum([]byte(str)), i)] = str
+		}
+	}
+	for shortStr, longStr := range substitutions {
+		commandline = strings.Replace(commandline, longStr, shortStr, -1)
+	}
+	// END Uber super hack
+
 	// This is a bit hacky, but we couldn't think of a better way to do it.
 	// We create a bash script and in that file we run a bash command that parses the
 	// command line arguments we wrote to the file. The bash script outputs each of the
@@ -50,5 +69,16 @@ func ParseArgs(commandline string) ([]string, error) {
 	// Remove the last element of the argsArray because the output ends with an endline
 	// and has an empty last element
 	argsArray = argsArray[0 : len(argsArray)-1]
+
+	// BEGIN Uber super hack part duex
+	for i, _ := range argsArray {
+		for shortStr, longStr := range substitutions {
+			argsArray[i] = strings.Replace(argsArray[i], shortStr, longStr, -1)
+		}
+	}
+	// END Uber super hack part duex
+
+	// fmt.Println(argsArray)
+
 	return argsArray, nil
 }
--- argsparser/argsparser_test.go
+++ argsparser/argsparser_test.go
@@ -1,14 +1,27 @@
 package argsparser
 
 import (
+	"fmt"
+	"io/ioutil"
 	"strconv"
+	"strings"
 	"testing"
 )
 
-// Helper function to assert that two strings are equal
-func checkStringsEqual(t *testing.T, expected string, actual string) {
-	if expected != actual {
-		t.Fatal("Actual response: " + actual + " does not match expected: " + expected)
+func assertEqual(t *testing.T, expected, actual interface{}, message string) {
+	message += fmt.Sprintf("\nExpected: '%s', got: '%s'", expected, actual)
+	assert(t, expected == actual, message)
+}
+
+func assert(t *testing.T, assertion bool, message string) {
+	if !assertion {
+		t.Fatal(message)
+	}
+}
+
+func assertIfErr(t *testing.T, err error, message string) {
+	if err != nil {
+		assert(t, false, fmt.Sprintf("%s: %s", message, err.Error()))
 	}
 }
 
@@ -20,8 +33,22 @@ func TestParseArgs(t *testing.T) {
 	if len(argsArray) != 4 {
 		t.Fatal("Args length = " + strconv.Itoa(len(argsArray)) + ", 4 expected")
 	}
-	checkStringsEqual(t, "arg with quotes", argsArray[0])
-	checkStringsEqual(t, "secondArg", argsArray[1])
-	checkStringsEqual(t, "thirdArg", argsArray[2])
-	checkStringsEqual(t, "another with quotes", argsArray[3])
+	message := "Unexpected parsed argument"
+	assertEqual(t, "arg with quotes", argsArray[0], message)
+	assertEqual(t, "secondArg", argsArray[1], message)
+	assertEqual(t, "thirdArg", argsArray[2], message)
+	assertEqual(t, "another with quotes", argsArray[3], message)
+}
+
+func TestLongArgs(t *testing.T) {
+	rawData, err := ioutil.ReadFile("/Users/sparks/dev/baseworker-go/argsparser/long_args.txt")
+	assertIfErr(t, err, "Could not parse test data file")
+	commandline := strings.Replace(string(rawData), "\n", "", -1)
+	expectedArgs := strings.Split(commandline, " ")
+	parsedArgs, err := ParseArgs(commandline)
+	assertIfErr(t, err, "Error during ParseArgs")
+	assertEqual(t, len(expectedArgs), len(parsedArgs), "Wrong number of arguments")
+	for i, _ := range expectedArgs {
+		assertEqual(t, expectedArgs[i], parsedArgs[i], "Unexpected parsed argument"+strconv.Itoa(i))
+	}
 }
